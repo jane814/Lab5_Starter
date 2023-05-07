b022a86d8eaaabefd294a7bd901676d7d87ef1ab
jane814 <63211137+jane814@users.noreply.github.com>
2023-05-07 16:30:27 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,8 +4,9 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   var selected = document.querySelector(".horn-select");
-  selected.addEventListener('change', (event) = {
-    var changed = document.querySelector('[alt="No image selected"] img')
-    changed.src = "assets/images/${event.target.value}.svg"
-  }
+  selected.addEventListener('change', (event) => {
+    var changed = document.querySelector("img");
+    changed.src = `./assets/images/${event.target.value}`;
+    console.log(changed.src);
+  });
 }
