d45cdc72b2b59527f1a55bc3caf5ea03b25ddc33
jane814 <63211137+jane814@users.noreply.github.com>
2023-05-07 17:31:18 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,9 +4,9 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   var selected = document.getElementById("horn-select");
+  var changed = document.querySelector("img");
+  var audio_set = document.getElementsByClassName("hidden");
   selected.addEventListener('change', (event) => {
-    var changed = document.querySelector("img");
-    var audio_set = document.getElementsByClassName("hidden");
     audio_set.src = `./assets/audio/${event.target.value}.mp3`;
     changed.src = `./assets/images/${event.target.value}.svg`;
   });
