1d33fac8232d7460ffd83048c5d5366497f5c84e
jane814 <63211137+jane814@users.noreply.github.com>
2023-05-07 20:32:24 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,6 +7,9 @@ function init() {
   var changed = document.querySelector("img");
   var audio_set = document.querySelector("audio");
   var pressed = document.querySelector("button");
+  var level = document.getElementById("volume");
+  var images = document.querySelectorAll("img");
+  var loud = images[1];
   selected.addEventListener('change', (event) => {
     audio_set.src = `./assets/audio/${event.target.value}.mp3`;
     changed.src = `./assets/images/${event.target.value}.svg`;
@@ -14,4 +17,22 @@ function init() {
   pressed.addEventListener('click',(event) => {
     audio_set.play();
   });
+  level.addEventListener('input',(event) => {
+    if(event.target.value ==0){
+      audio_set.volume = 0.0;
+      loud.src = "./assets/icons/volume-level-0.svg";
+    }
+    else if(1 <= event.target.value <33){
+      audio_set.volume = 0.33;
+      loud.src = "./assets/icons/volume-level-1.svg";
+    }
+    else if(33 <= event.target.value <67){
+      audio_set.volume = 0.66;
+      loud.src = "./assets/icons/volume-level-2.svg";
+    }
+    else{
+      audio_set.volume = 1.0;
+      loud.src = "./assets/icons/volume-level-3.svg"; 
+    }
+  });  
 }
