5a5c8859ec14c5cec782f2415a65dcdf3990c8de
jane814 <63211137+jane814@users.noreply.github.com>
2023-05-07 21:01:09 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -23,11 +23,11 @@ function init() {
       audio_set.volume = 0.0;
       loud.src = "./assets/icons/volume-level-0.svg";
     }
-    else if(1 <= num <33){
+    else if(1 <= num && num<33){
       audio_set.volume = 0.33;
       loud.src = "./assets/icons/volume-level-1.svg";
     }
-    else if(33 <= num <67){
+    else if(33 <= num && num<67){
       audio_set.volume = 0.66;
       loud.src = "./assets/icons/volume-level-2.svg";
     }
